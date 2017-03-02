8f1329ecf0db30fc14f7a6904203b63df443753c
callam <dczekala@googlemail.com>
2017-03-02 17:58:42 +0100

    Aktueller Stand

--- install-extensions.cmd
+++ install-extensions.cmd
@@ -2,8 +2,10 @@ call code --install-extension adamhartford.vscode-base64
 call code --install-extension alefragnani.Bookmarks
 call code --install-extension bibhasdn.git-easy
 call code --install-extension dbankier.vscode-instant-markdown
+call code --install-extension dbankier.vscode-quick-select
 call code --install-extension donjayamanne.githistory
 call code --install-extension DotJoshJohnson.xml
+call code --install-extension eamodio.find-related
 call code --install-extension emmanuelbeziat.vscode-great-icons
 call code --install-extension felipecaputo.git-project-manager
 call code --install-extension formulahendry.code-runner
@@ -12,6 +14,8 @@ call code --install-extension luggage66.AWK
 call code --install-extension lukehoban.Go
 call code --install-extension nemesv.copy-file-name
 call code --install-extension pprice.better-merge
+call code --install-extension ryu1kn.text-marker
+call code --install-extension SirTobi.code-clip-ring
 call code --install-extension steve8708.Align
 call code --install-extension wmaurer.change-case
 call code --install-extension xyz.plsql-language
