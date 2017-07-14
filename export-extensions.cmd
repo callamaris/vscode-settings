ff5799a85440d2603f4139c01e0b7be99ff12738
Callamaris <dczekala@googlemail.com>
2017-07-14 19:08:32 +0200

    Vorher die Extensions löschen

--- export-extensions.cmd
+++ export-extensions.cmd
@@ -2,6 +2,8 @@
 call code --list-extensions > list.txt
 
 REM @echo off
-(for /f "delims=" %%L in (list.txt) do @echo call code --install-extension %%L)> install-extensions.cmd
+
+echo FOR /D %%%%p IN ("%%USERPROFILE%%\.vscode\extensions\*.*") DO rmdir "%%%%p" /s /q> install-extensions.cmd
+(for /f "delims=" %%L in (list.txt) do @echo call code --install-extension %%L)>> install-extensions.cmd
 
 del list.txt
