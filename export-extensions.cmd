a4497ded43d2bc062fa59c7644515779ab2209c6
dennis <dczekala@googlemail.com>
2018-03-27 20:43:11 +0200

    Extensions Installieren auch für Linux

--- export-extensions.cmd
+++ export-extensions.cmd
@@ -6,4 +6,7 @@ REM @echo off
 echo FOR /D %%%%p IN ("%%USERPROFILE%%\.vscode\extensions\*.*") DO rmdir "%%%%p" /s /q> install-extensions.cmd
 (for /f "delims=" %%L in (list.txt) do @echo call code --install-extension %%L)>> install-extensions.cmd
 
+echo rm -rf ~/.vscode/extensions> install-extensions.sh
+(for /f "delims=" %%L in (list.txt) do @echo code --install-extension %%L)>> install-extensions.sh
+
 del list.txt
